2946cda95b2c501053ee415207303e02e291dbef
Alex Wulf <aw@dc.ag>
2023-01-28 14:19:00 +0100

    Renamed

--- generate.sh
+++ generate.sh
@@ -2,6 +2,6 @@
 docker run --rm \
   -v ${PWD}:/local openapitools/openapi-generator-cli generate \
   -i https://docs.docker.com/engine/api/v1.41.yaml \
-  --additional-properties disallowAdditionalPropertiesIfNotPresent=false,invokerPackage=Wulfheart\\DockerApi \
+  --additional-properties disallowAdditionalPropertiesIfNotPresent=false,invokerPackage=Wulfheart\\Docker \
   -g php \
   -o /local/
